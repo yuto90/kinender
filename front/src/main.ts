0cb8c8586d652333cfa394a97498f2fec8c3a940
yuto90 <solidusgako4087@gmail.com>
2021-11-09 09:23:35 +0900

    tailwind css導入

--- front/src/main.ts
+++ front/src/main.ts
@@ -5,5 +5,6 @@ import router from "./router";
 import { store, key } from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import "./index.css";
 
 createApp(App).use(store, key).use(router).use(VueAxios, axios).mount("#app");
